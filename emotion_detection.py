929f83198bb958f43bac46c7b5c9d05a0cb2148c
AbeVallerian <abe.vallerian@gmail.com>
2024-07-15 12:55:07 -0400

    modify script

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,11 +1,16 @@
 import json
 import requests
 
-def emotion_detector(text_to_analyse):
+def emotion_detector(text_to_analyze):
     url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
-    myobj = { "raw_document": { "text": text_to_analyse } }
+    myobj = {
+        "raw_document": {
+            "text": text_to_analyze
+        }
+    }
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(url, json = myobj, headers=header)
+
     emotion_predictions = json.loads(response.text)["emotionPredictions"]
     emotions = {
         "anger": emotion_predictions[0]["emotion"]["anger"],
@@ -14,7 +19,7 @@ def emotion_detector(text_to_analyse):
         "joy": emotion_predictions[0]["emotion"]["joy"],
         "sadness": emotion_predictions[0]["emotion"]["sadness"],
     }
-    
+
     top_score = 0.0
     top_emotion = ""
     for emotion, score in emotions.items():
@@ -22,4 +27,5 @@ def emotion_detector(text_to_analyse):
             top_score = score
             top_emotion = emotion
     emotions["dominant_emotion"] = top_emotion
+
     return emotions
